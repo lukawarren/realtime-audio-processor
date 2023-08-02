bed85388b61e44db388bacacf45c40240f6d79a8
Luka Warren <lukawarren1@gmail.com>
2023-08-02 18:31:51 +0100

    Bass tweaks

--- src/effects/bass_effect.cpp
+++ src/effects/bass_effect.cpp
@@ -15,50 +15,53 @@ void BassEffect::ApplyEffect(Packet& packet)
         packet.current_samples.size() != packet.next_samples.size())
         return;
 
-    std::array<std::vector<float>, 3> windows = CreateWindows(packet);
-
-    // Remove current samples
-    for (float& sample : packet.current_samples)
-        sample = 0.0f;
-
-    for (size_t i = 0; i < 3; ++i)
+    for (int i = 0; i < 2; ++i)
     {
-        // Perform FFT to convert to frequency domain
-        FastFourierTransform fft(windows[i], std::nullopt);
-        std::vector<std::complex<float>>& fft_output = fft.output;
+        std::array<std::vector<float>, 3> windows = CreateWindows(packet);
 
-        PerformBassBoost(fft_output, packet.frequency);
+        // Remove current samples
+        for (float& sample : packet.current_samples)
+            sample = 0.0f;
 
-        // Perform IFFT to convert back to time domain
-        InverseFourierTransform inverse(fft_output);
-        std::vector<float> scaled_real_components;
-        scaled_real_components.reserve(windows[i].size());
-        for (const auto& c : inverse.output)
-            scaled_real_components.emplace_back(
-                c.real() / (float)inverse.output.size()
-            );
-
-        // Write to current output
-        const size_t half_size = scaled_real_components.size() / 2;
-        for (size_t j = 0; j < half_size * 2; ++j)
+        for (size_t i = 0; i < 3; ++i)
         {
-            // First window
-            if (i == 0 && j > half_size)
-                packet.current_samples[j - half_size] += scaled_real_components[j];
+            // Perform FFT to convert to frequency domain
+            FastFourierTransform fft(windows[i], std::nullopt);
+            std::vector<std::complex<float>>& fft_output = fft.output;
 
-            // Second window
-            else if (i == 1)
-                packet.current_samples[j] += scaled_real_components[j];
+            PerformBassBoost(fft_output, packet.frequency);
 
-            // Third window
-            else if (i == 2 && j < half_size)
-                packet.current_samples[j + half_size] += scaled_real_components[j];
+            // Perform IFFT to convert back to time domain
+            InverseFourierTransform inverse(fft_output);
+            std::vector<float> scaled_real_components;
+            scaled_real_components.reserve(windows[i].size());
+            for (const auto& c : inverse.output)
+                scaled_real_components.emplace_back(
+                    c.real() / (float)inverse.output.size()
+                );
+
+            // Write to current output
+            const size_t half_size = scaled_real_components.size() / 2;
+            for (size_t j = 0; j < half_size * 2; ++j)
+            {
+                // First window
+                if (i == 0 && j > half_size)
+                    packet.current_samples[j - half_size] += scaled_real_components[j];
+
+                // Second window
+                else if (i == 1)
+                    packet.current_samples[j] += scaled_real_components[j];
+
+                // Third window
+                else if (i == 2 && j < half_size)
+                    packet.current_samples[j + half_size] += scaled_real_components[j];
+            }
         }
-    }
 
-    // Samples will each have been "added to" multiple times so we must reduce the result
-    for (float& sample : packet.current_samples)
-        sample /= 4.0f;
+        // Samples will each have been "added to" multiple times so we must reduce the result
+        for (float& sample : packet.current_samples)
+            sample /= 4.0f;
+    }
 }
 
 std::array<std::vector<float>, 3> BassEffect::CreateWindows(Packet& packet) const
@@ -103,7 +106,7 @@ void BassEffect::PerformBassBoost(
 ) const
 {
     const float lower_frequency = 20.0f;
-    const float upper_frequency = 250.0f;
+    const float upper_frequency = 500.0f;
     const float frequency_resolution = (float)frequency / (float)fft_output.size();
 
     // Work out "bins"
@@ -116,9 +119,8 @@ void BassEffect::PerformBassBoost(
         float magnitude = std::abs(fft_output[i]);
         float phase = std::arg(fft_output[i]);
 
-        // Apply bass effect to the magnitude
-        magnitude *= 0.01f;
-        magnitude *= 0.01f;
+        // Remove bass
+        magnitude = 0.0f;
 
         // Convert back to Cartesian form
         fft_output[i] = std::polar(magnitude, phase);
@@ -132,5 +134,5 @@ std::string BassEffect::GetName() const
 
 std::vector<std::string> BassEffect::GetPropertyNames() const
 {
-    return { "Minimum frequency", "Maximum frequency", "Amount" };
+    return { "Minimum frequency", "Maximum frequency", "Iterations" };
 }
